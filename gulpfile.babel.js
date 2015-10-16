3027ab37382158de5ce94c6b6268dd255da60d7c
Joshua Briley <josh@thebrileys.com>
2015-10-15 23:36:51 -0400

    Initial markup and styling of quiz items.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -24,8 +24,29 @@ gulp.task('styles', () => {
     .pipe(reload({stream: true}));
 });
 
+// function lint(files, options) {
+//   return () => {
+//     return gulp.src(files)
+//       .pipe(reload({stream: true, once: true}))
+//       .pipe($.eslint(options))
+//       .pipe($.eslint.format())
+//       .pipe($.if(!browserSync.active, $.eslint.failAfterError()));
+//   };
+// }
+// const testLintOptions = {
+//   env: {
+//     mocha: true
+//   }
+// };
 
+gulp.task('deploy', ['build'], () => {
+  return gulp.src('dist')
+    .pipe($.subtree())
+    .pipe($.clean());
+});
 
+//gulp.task('lint', lint('app/scripts/**/*.js'));
+//gulp.task('lint:test', lint('test/spec/**/*.js', testLintOptions));
 
 gulp.task('html', ['styles'], () => {
   const assets = $.useref.assets({searchPath: ['.tmp', 'app', '.']});
@@ -75,12 +96,6 @@ gulp.task('extras', () => {
 
 gulp.task('clean', del.bind(null, ['.tmp', 'dist']));
 
-gulp.task('deploy', ['build'], () => {
-  return gulp.src('dist')
-    .pipe($.subtree())
-    .pipe($.clean());
-});
-
 gulp.task('serve', ['styles', 'fonts'], () => {
   browserSync({
     notify: false,
@@ -129,7 +144,7 @@ gulp.task('serve:test', () => {
   });
 
   gulp.watch('test/spec/**/*.js').on('change', reload);
-  //gulp.watch('test/spec/**/*.js', ['lint:test']);
+  gulp.watch('test/spec/**/*.js', ['lint:test']);
 });
 
 // inject bower components
